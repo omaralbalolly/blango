26a5786f3684b1502b5029a8a0322ce99cade917
Omar Albalouli <albalollyomar@gmail.com>
2021-11-07 03:33:11 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 import blog.views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", blog.views.index)
+    path("", blog.views.index),
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
 ]
--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,13 +3,37 @@ from django import template
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
+from blog.models import Post
 user_model = get_user_model()
 register = template.Library()
-@register.filter
-def author_details(author, current_user=None):
-    if not isinstance(author, user_model):
-        # return empty string as safe default
-        return ""
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
+
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
 
     if author == current_user:
         return format_html("<strong>me</strong>")
@@ -26,4 +50,4 @@ def author_details(author, current_user=None):
         prefix = ""
         suffix = ""
 
-    return format_html('{}{}{}', prefix, name, suffix)
+    return format_html("{}{}{}", prefix, name, suffix)
