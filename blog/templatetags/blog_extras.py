db8ac08385de48f83ed32784b4c54d8fc04f8f82
Omar Albalouli <albalollyomar@gmail.com>
2021-11-08 04:16:38 +0000

    Finish Caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -4,8 +4,10 @@ from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
 from blog.models import Post
+import logging
 user_model = get_user_model()
 register = template.Library()
+logger = logging.getLogger(__name__)
 
 @register.simple_tag
 def row(extra_classes=""):
@@ -26,6 +28,7 @@ def endcol():
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
     return {"title": "Recent Posts", "posts": posts}
 
 @register.simple_tag(takes_context=True)
