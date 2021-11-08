96ae4a972f78aac28b893f587771543800b4cc59
Omar Albalouli <albalollyomar@gmail.com>
2021-11-08 13:23:30 +0000

    Django Registration

--- blango/settings.py
+++ blango/settings.py
@@ -199,6 +199,8 @@ class Dev(Configuration):
     
     INTERNAL_IPS = ["192.168.10.226"]
     AUTH_USER_MODEL = "blango_auth.User"
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    ACCOUNT_ACTIVATION_DAYS = 7
 
 class Prod(Dev):
     DEBUG = False
--- blango/urls.py
+++ blango/urls.py
@@ -17,8 +17,12 @@ import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-
+import blango_auth.views
 import blog.views
+
+from django_registration.backends.activation.views import RegistrationView
+from blango_auth.forms import BlangoRegistrationForm
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
@@ -29,4 +33,12 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
+        path("accounts/", include("django.contrib.auth.urls")),
+        path("accounts/profile/", blango_auth.views.profile, name="profile"),
+        path(
+            "accounts/register/",
+            RegistrationView.as_view(form_class=BlangoRegistrationForm),
+            name="django_registration_register",
+        ),
+        path("accounts/", include("django_registration.backends.activation.urls")),
     ]
