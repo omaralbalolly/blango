ea9122ea0ef8775efafcddd0cada9a073ba7e039
Omar Albalouli <albalollyomar@gmail.com>
2021-11-10 04:08:37 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -57,6 +57,7 @@ class Dev(Configuration):
         'allauth.account',
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
+        'rest_framework',
     ]
 
     MIDDLEWARE = [
