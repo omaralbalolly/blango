0695d13caab87d95788b2d05b1a2d1466bb1a7ae
Omar Albalouli <albalollyomar@gmail.com>
2021-11-12 08:34:07 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -213,6 +213,14 @@ class Dev(Configuration):
     ACCOUNT_EMAIL_REQUIRED = True
     ACCOUNT_USERNAME_REQUIRED = False
     ACCOUNT_AUTHENTICATION_METHOD = "email"
+    
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ]
+    }
 
 class Prod(Dev):
     DEBUG = False
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken import views
 
 from blog.api.views import PostList, PostDetail
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    path("auth/", include("rest_framework.urls")),
+    path("token-auth/", views.obtain_auth_token),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
