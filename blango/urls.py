6cf61b2db5bc38f58e9f03a826969ee2e7f8cc3e
Omar Albalouli <albalollyomar@gmail.com>
2022-06-30 18:33:15 +0000

    Finish JavaScript intro and fundamentals

--- blango/urls.py
+++ blango/urls.py
@@ -28,7 +28,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
-    path("ip/", blog.views.get_ip)
+    path("ip/", blog.views.get_ip),
+    path("post-table/", blog.views.post_table, name="blog-post-table"),
 ]
 
 if settings.DEBUG:
--- blog/views.py
+++ blog/views.py
@@ -34,7 +34,8 @@ def post_detail(request, slug):
     return render(
         request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
     )
-
+def post_table(request):
+    return render(request, "blog/post-table.html")
 def get_ip(request):
   from django.http import HttpResponse
   return HttpResponse(request.META['REMOTE_ADDR'])
